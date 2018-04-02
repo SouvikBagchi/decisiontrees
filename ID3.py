fa2699f8f2432d6cf3ae910dc3e1f8046f0c1b4f
souvikbagchi <souvik.bagchi@gmail.com>
2018-04-03 02:05:58 +0530

    ID3 calculate_gain added

--- ID3.py
+++ ID3.py
@@ -36,13 +36,17 @@ class ID3():
     self.no_of_positive_outcomes+=1
   #SET THE NEGATIVE OUTCOMES
   self.no_of_negative_outcomes = self.training_set_len-self.no_of_positive_outcomes
-  #GET THE ENTROPY OF THE LIST
-  self.entropy = 0
+  #DECLARE THE ENTROPY VARIABLE OF THE LIST
+  self.entropy = self.calculate_entropy(self.no_of_positive_outcomes,self.no_of_negative_outcomes)
 
   #Set the tree length
   self.tree_length = self.num_attributes-1
   #INITIALIZE AN EMPTY TREE
   self.tree = {}
+
+ #THIS METHOD STARTS TO MAKE THE DECISION TREE
+ def make_tree(self):
+  
 
  def test(self):
   print("TEST RUNNING")
@@ -66,31 +70,41 @@ class ID3():
   
   return(s)
 
- #THIS METHOD WILL CALCULATE THE GAIN	
- def calculate_gain(self,total_entropy, len_training_set, attribute_list,outcome_list):
+ #THIS METHOD WILL CALCULATE THE GAIN
+ #IT TAKES THREE ARGUMENTS - len_training_set, attribute_list,outcome_list
+ #IT CALCULATES THE GAIN FOR THE SET AND RETURNS THE VALUE
+ def calculate_gain(self,attribute_list,outcome_list):
   #GET THE UNIQUE ATTRIBUTES
-  attributes = []
+  attribute_values = []
   for attr in set(attribute_list):
-  	attributes.append(attr)
-  #CREATE A DICT OF ATTRIBUTE
-  attribute_dict = {}
-  for i in attributes :
-  	attribute_dict[str(i)] = []
+  	attribute_values.append(attr)
+  #CREATE A DICT OF ATTRIBUTE WHICH HOLDS NO OF POSITIVE AND NEGATIVE OUTCOME
+  pos_attribute_value_dict = {}
+  neg_attribute_value_dict = {}
+  for i in attribute_values :
+   pos_attribute_value_dict[str(i)] = 0
+   neg_attribute_value_dict[str(i)] = 0
   #LOOP THROUGH THE OUTCOME
-  
-  #INITIALIIZE GAIN WITH total_entropy
-  gain = total_entropy
-  #LOOP LENGHT OF ATTRIBUTE VALUES TO SUBTRACT THE CORR SV/S
-  for i in range(0,len_training_set):
-   gain -= 1
+  for i in range(1,len(outcome_list)):
+   #START CHECKING FROM THE 2ND ELEMENT SINCE THE FIRST ELEMENT IS THE COLUMN NAME
+   if outcome_list[i] == self.positive_outcome : 
+    #CHECK THE NAME OF THE ATTRIBUTE AT THAT INDEX IN THE ATTRIBUTE LIST AND APPEND IN THE DICT
+    pos_attribute_value_dict[str(attribute_list[i])]+=1
+   #ELSE NEGATIVE, INCREMENT THE NEGATIVE DICT
+   else :
+   	neg_attribute_value_dict[str(attribute_list[i])]+=1
 
- #THIS METHOD STARTS TO MAKE THE DECISION TREE
- def make_tree(self):
-  self.entropy = self.calculate_entropy(self.no_of_positive_outcomes,self.no_of_negative_outcomes)
-  #LOOP TREE LENGTH
-  for i in range(0,self.tree_length):
-   #CALCULATE GAIN FOR ALL ATTRIBUTES
-   gains = []
+  #INITIALIIZE GAIN WITH total_entropy
+  gain = self.entropy
+  #LOOP LENGHT OF ATTRIBUTE VALUES TO SUBTRACT THE CORR SV/S
+  for attribute_value in attribute_values:
+   gain -= (pos_attribute_value_dict[attribute_value]+
+   	neg_attribute_value_dict[attribute_value])*self.calculate_entropy(pos_attribute_value_dict[attribute_value],
+   	neg_attribute_value_dict[attribute_value])
+
+  return(gain)
+
+
  
 
 training_list = [
