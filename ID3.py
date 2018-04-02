474fe8a615be5cb4092a1d9796798af0ea3fbae2
souvikbagchi <souvik.bagchi@gmail.com>
2018-04-03 00:34:12 +0530

    ID3 entropy tested and executed

--- ID3.py
+++ ID3.py
@@ -1,4 +1,6 @@
+from __future__ import division
 import math
+
 class ID3():
  #INITIALIZE THE CLASS WITH THE INPUT OF OUTCOME AND TRAINING ATTRIBUTE LIST
  def __init__(self,training_list,positive_outcome):
@@ -22,39 +24,50 @@ class ID3():
     temp_list.append(self.training_list[j+1][i]) #rowsxcolumns
    #CHANGE THE OLD ATTRIBUTE LIST 
    self.attribute_list[i] = temp_list
-
   #STORE OUTCOME IN A SEPARATE LIST AND REMOVE FROM ATTRIBUTE LIST
   self.outcome_list = self.attribute_list[-1]
   del self.attribute_list[-1]
+  #CREATE VARIABLES WHICH HOLD THE NUMBER OF POSITIVE AND NEGATIVE OUTCOMES
+  self.no_of_positive_outcomes = 0
+  self.no_of_negative_outcomes = 0
+  #COUNT THE POSITIVE OUTCOMES AND SET IT
+  for item in self.outcome_list:
+   if item == self.positive_outcome :
+    self.no_of_positive_outcomes+=1
+  #SET THE NEGATIVE OUTCOMES
+  self.no_of_negative_outcomes = self.training_set_len-self.no_of_positive_outcomes
+  #GET THE ENTROPY OF THE LIST
+  self.entropy = 0
 
   #Set the tree length
   self.tree_length = self.num_attributes-1
   #INITIALIZE AN EMPTY TREE
   self.tree = {}
 
+ def test(self):
+  print("TEST RUNNING")
+  self.entropy = 9
+  print("END TEST")
 
- #THIS METHOD STARTS TO MAKE THE DECISION TREE
- def make_tree():
-  #LOOP TREE LENGTH
-  for i in range(0,self.tree_length):
-   #CALCULATE GAIN FOR ALL ATTRIBUTES
-   gains = []
-   for attr in self.attribute_lists:
-    gains.append(gain(attr))
- 
  #THIS METHOD WILL CALCULATE THE ENTROPY
- def entropy(no_of_positive_outcomes,no_of_negative_outcomes):
-
+ def calculate_entropy(self,num_positive_outcomes,num_negative_outcomes):
+  print("+ve : "+str(num_positive_outcomes)+" -ve : "+str(num_negative_outcomes))
+  #DEFINE THE PROPORTIONS
+  pos_proportion = 0.0
+  neg_proportion = 0.0
   #CALCULATE THE PROPORTIONS OF POSITIVE AND NEGATIVE
-  pos_proportion = no_of_positive_outcomes/(no_of_positive_outcomes+no_of_negative_outcomes)
-  neg_proportion = no_of_negative_outcomes/(no_of_positive_outcomes+no_of_negative_outcomes)
+  pos_proportion = num_positive_outcomes/(num_positive_outcomes+num_negative_outcomes)
+  neg_proportion = num_negative_outcomes/(num_positive_outcomes+num_negative_outcomes)
+  #COMMENT ON PRODUCTION
+  print("+ve pro : "+str(pos_proportion)+", -ve ro : "+str(neg_proportion))
 
   #ENTROPY CALCULATION
-  s = - pos_proportion*(math.log(pos_proportion,2)) - neg_proportion*(math.log(neg_proportion,2))
+  s = -(pos_proportion*(math.log(pos_proportion,2))) -(neg_proportion*(math.log(neg_proportion,2)))
+  
   return(s)
 
  #THIS METHOD WILL CALCULATE THE GAIN	
- def gain(total_entropy, len_training_set, attribute_list,outcome_list):
+ def calculate_gain(self,total_entropy, len_training_set, attribute_list,outcome_list):
   #GET THE UNIQUE ATTRIBUTES
   attributes = []
   for attr in set(attribute_list):
@@ -64,7 +77,6 @@ class ID3():
   for i in attributes :
   	attribute_dict[str(i)] = []
   #LOOP THROUGH THE OUTCOME
-
   
   #INITIALIIZE GAIN WITH total_entropy
   gain = total_entropy
@@ -72,7 +84,23 @@ class ID3():
   for i in range(0,len_training_set):
    gain -= 1
 
+ #THIS METHOD STARTS TO MAKE THE DECISION TREE
+ def make_tree(self):
+  self.entropy = self.calculate_entropy(self.no_of_positive_outcomes,self.no_of_negative_outcomes)
+  #LOOP TREE LENGTH
+  for i in range(0,self.tree_length):
+   #CALCULATE GAIN FOR ALL ATTRIBUTES
+   gains = []
+ 
 
-training_list = [['Outlook','Temperature','outcome'],['Sunny','Hot','Yes'],['Rain','Warm','Yes'],['Sunny','Cool','No']]
+training_list = [
+['Outlook','Temperature','outcome'],
+['Sunny','Hot','Yes'],
+['Rain','Warm','Yes'],
+['Sunny','Cool','No'],
+['Cloudy','Cool','Yes'],
+['Rain','Cool','No']
+]
 x = training_list
-y = ID3(training_list,'Yes')
+y = ID3(x,'Yes')
+y.make_tree()
